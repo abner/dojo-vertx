089c4e40a766201cd27deb91ee003c15b244c0ae
Bruno Guimaraes <bruno.guimaraes@serpro.gov.br>
2016-10-17 17:38:58 -0300

    Commit inicial

---
+++ src/main/java/Bookmark.java
@@ -0,0 +1,43 @@
+
+public class Bookmark {
+
+	private Integer id;
+
+	private String nome;
+
+	private String url;
+
+	public Bookmark() {
+	}
+
+	public Bookmark(Integer id, String nome, String url) {
+		this.id = id;
+		this.nome = nome;
+		this.url = url;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $br.gov.serpro.vertx.br.gov.serpro.vertxapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
